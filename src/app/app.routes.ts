cbad0bdd956fad892d4d909f5975f2bf6615cf52
GamaAnder <anderson.a.jose@gmail.com>
2026-09-19 08:34:45 -0600

    se ajusta examen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Login } from './login/login';
 import { Bienvenida } from './bienvenida/bienvenida';
 import { Anuncio } from './anuncio/anuncio';
+import { Examen } from './examen/examen';
 
 export const routes: Routes = [
     {path:"", component:Login},
     {path:"bienvenida", component:Bienvenida},
+    {path:"anuncio", component: Examen},
     {path:"anuncio", component: Anuncio}
 ];
